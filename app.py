11365c9a5a6ba7b5749e82fc853343f95cfd5e2f
jimbog <jimmygarzon@gmail.com>
2014-06-05 13:07:45 -0500

    fix link to show picture

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def products():
 @app.route('/product/<int:product_id>')
 def product(product_id):
     product = [{'id':1, 'name': "Product 1", 'description': "This is the best prodcut ever!",
-                'image': "https://www.dropbox.com/sh/ujpqpimyfnvhney/AACZX5QcAVmsTQ2lrgkg5FOGa/christian_purple_pump.jpg"},
+        'image': "https://photos-4.dropbox.com/t/0/AAC7yG8Y93UAB8uc8uw5F-IlTSVpAJKRxe1wVye7ydcj_g/12/97133399/jpeg/2048x1536/3/1401998400/0/2/christian_purple_pump.jpg/2bS1eTy3aB3-8Fpz52070SU9FYEExqrKkbQXQSssffQ"},
                {'id':2, 'name': "Product 2", 'description': "This is the second best prodcut ever!" },
                {'id':3, 'name': "Product 3", 'description': "This is the third best prodcut ever!" },
                {'id':4, 'name': "Product 4", 'description': "This is the fourth best prodcut ever!" },
